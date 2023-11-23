fdaf3df1128acc6acbaf94dd9d805d2a75062316
Jan Hák <jan.hak@windowslive.com>
2023-11-23 20:49:51 +0100

    Fix BPF filter

--- src/libuis/kern/bpf.c
+++ src/libuis/kern/bpf.c
@@ -1,9 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/in.h>
 #include <linux/ip.h>
+#include <linux/ipv6.h>
+#include <linux/tcp.h>
 #include <linux/udp.h>
+
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
@@ -57,13 +61,20 @@ typedef struct {
 	void *begin;
 } view_t;
 
-static __always_inline 
-unsigned short parse_ethhdr(view_t *view, struct ethhdr **ethhdr)
+static __always_inline
+void stats_add(__u32 code, __u64 bytes)
 {
-	if (ethhdr == NULL) {
-		return -1;
+	struct datarec *rec = bpf_map_lookup_elem(&xdp_stats_map, &code);
+	if (rec == NULL) {
+		return;
 	}
+	rec->rx_packets++;
+	rec->rx_bytes += bytes;
+}
 
+static __always_inline 
+int parse_ethhdr(view_t *view, struct ethhdr **ethhdr)
+{
 	struct ethhdr *eth = view->begin;
 	int hdrsize = sizeof(*eth);
 	if (view->begin + hdrsize > view->end) {
@@ -73,20 +84,18 @@ unsigned short parse_ethhdr(view_t *view, struct ethhdr **ethhdr)
 	view->begin += hdrsize;
 	*ethhdr = eth;
 
-	return eth->h_proto;
+	return 0;
 }
 
 static __always_inline
 int parse_iphdr(view_t *view, struct iphdr **iphdr)
 {
-	if (view == NULL || iphdr == NULL) {
-		return -1;
-	}
-	int hdrsize = sizeof(*iphdr);
+	struct iphdr *iph = view->begin;
+	int hdrsize = sizeof(*iph);
 	if (view->begin + hdrsize > view->end) {
 		return -1;
 	}
-	struct iphdr *iph = view->begin;
+	// Validate IPv4
 	if (iph->version != 4 || iph->ihl < 5) {
 		return -1;
 	}
@@ -99,26 +108,31 @@ int parse_iphdr(view_t *view, struct iphdr **iphdr)
 	view->begin += hdrsize;
 	*iphdr = iph;
 
-	return iph->protocol;
+	return 0;
 }
 
-// static __always_inline int parse_ip6hdr(view_t *view, struct ipv6hdr **ip6hdr)
-// {
-// 	struct ipv6hdr *ip6h = view->begin;
-
-// 	if (ip6h + 1 > data_end)
-// 		return -1;
-
-// 	nh->pos = ip6h + 1;
-// 	*ip6hdr = ip6h;
-
-// 	return skip_ip6hdrext(nh, data_end, ip6h->nexthdr);
-// }
-
-
-static __always_inline int parse_udphdr(view_t *view, struct udphdr **udphdr)
+static __always_inline
+int parse_ip6hdr(view_t *view, struct ipv6hdr **ip6hdr)
 {
-	int len;
+	struct ipv6hdr *ip6h = view->begin;
+	int hdrsize = sizeof(*ip6h);
+	if (view->begin + hdrsize > view->end) {
+		return -1;
+	}
+	if (ip6h->version != 6) {
+		return -1;
+	}
+
+	view->begin += hdrsize;
+	*ip6hdr = ip6h;
+	
+	return 0;
+}
+
+
+static __always_inline
+int parse_udphdr(view_t *view, struct udphdr **udphdr)
+{
 	struct udphdr *udp = view->begin;
 	int hdrsize = sizeof(*udphdr);
 	if (view->begin + hdrsize > view->end)
@@ -127,83 +141,95 @@ static __always_inline int parse_udphdr(view_t *view, struct udphdr **udphdr)
 	view->begin += hdrsize;
 	*udphdr = udp;
 
-	len = bpf_ntohs(udp->len) - sizeof(struct udphdr);
-	if (len < 0)
+	return 0;
+}
+static __always_inline
+int parse_tcphdr(view_t *view, struct tcphdr **tcphdr)
+{
+	struct tcphdr *tcp = view->begin;
+	int hdrsize = sizeof(*tcphdr);
+	if (view->begin + hdrsize > view->end)
 		return -1;
 
-	return len;
+	view->begin += hdrsize;
+	*tcphdr = tcp;
+
+	return 0;
 }
 
 SEC("xdp")
 int xdp_filter(struct xdp_md *ctx)
 {
 	unsigned int rx_queue_idx = ctx->rx_queue_index;
-	struct datarec *rec;
 
 	view_t view = {
 		.end = (void *)(long)ctx->data_end,
 		.begin = (void *)(long)ctx->data
 	};
 
-	int eth_proto;
 	struct ethhdr *eth = NULL;
-	eth_proto = parse_ethhdr(&view, &eth);
-	if (eth_proto == -1) {
+	int ret = parse_ethhdr(&view, &eth);
+	if (ret) {
 		bpf_printk("Skip: Not ETH header!\n");
+		stats_add(XDP_PASS, ctx->data_end - ctx->data);
 		return XDP_PASS;
 	}
 
-	int ip_proto;
-	if (eth_proto == bpf_htons(ETH_P_IP)) {
+	unsigned char ip_proto;
+	if (eth->h_proto == bpf_htons(ETH_P_IP)) {
 		struct iphdr *ip = NULL;
-		ip_proto = parse_iphdr(&view, &ip);
-		if (ip_proto < 0) {
-			bpf_printk("Skip: Malformed IPv4 header!\n");
-			return XDP_PASS;
+		ret = parse_iphdr(&view, &ip);
+		if (ret) {
+			bpf_printk("Error: Malformed IPv4 header!\n");
+			stats_add(XDP_DROP, ctx->data_end - ctx->data);
+			return XDP_DROP;
 		}
-	} else if (eth_proto == bpf_htons(ETH_P_IPV6)) {
-		bpf_printk("Skip: IPv6 Header!\n");
-		return XDP_PASS;
+		ip_proto = ip->protocol;
+	} else if (eth->h_proto == bpf_htons(ETH_P_IPV6)) {
+		struct ipv6hdr *ipv6 = NULL;
+		ret = parse_ip6hdr(&view, &ipv6);
+		if (ret) {
+			bpf_printk("Error: Malformed IPv6 header!\n");
+			stats_add(XDP_DROP, ctx->data_end - ctx->data);
+			return XDP_DROP;
+		}
+		ip_proto = ipv6->nexthdr;
 	} else {
 		bpf_printk("Skip: Not IPvX Header!\n");
+		stats_add(XDP_PASS, ctx->data_end - ctx->data);
 		return XDP_PASS;
 	}
-	
-	if (ip_proto == IPPROTO_UDP || ip_proto == IPPROTO_TCP) {
-	// 	struct udphdr *udp;
-	// 	int ret = parse_udphdr(&view, &udp);
-	// 	if (ret <= 0) {
-	// 		bpf_printk("Skip: Not UDP header!\n");
-	// 		return XDP_PASS;
-	// 	}
-	// } else if (ip_proto == IPPROTO_TCP) {
-	// 	struct tcphdr *tcp;
-	// 	int ret = parse_tcphdr(&view, &tcp);
-	// 	if (ret < 0) {
-	// 		bpf_printk("Skip: Not TCP header!\n");
-	// 		return XDP_PASS;
-	// 	}
+
+	if (ip_proto == IPPROTO_UDP) {
+		struct udphdr *udp;
+		ret = parse_udphdr(&view, &udp);
+		if (ret) {
+			bpf_printk("Error: Malformed UDP header!\n");
+			stats_add(XDP_DROP, ctx->data_end - ctx->data);
+			return XDP_DROP;
+		}
+	} else if (ip_proto == IPPROTO_TCP) {
+		struct tcphdr *tcp;
+		int ret = parse_tcphdr(&view, &tcp);
+		if (ret < 0) {
+			bpf_printk("Error: Malformed TCP header!\n");
+			stats_add(XDP_DROP, ctx->data_end - ctx->data);
+			return XDP_DROP;
+		}
 	} else {
 		bpf_printk("Skip: Not UDP/TCP header!\n");
+		stats_add(XDP_PASS, ctx->data_end - ctx->data);
 		return XDP_PASS;
 	}
 
 	if (bpf_map_lookup_elem(&xsks_map, &rx_queue_idx)) {
-		int ret = bpf_redirect_map(&xsks_map, rx_queue_idx, 0);
-
-		rec = bpf_map_lookup_elem(&xdp_stats_map, &ret);
-		if (rec == NULL) {
-			bpf_printk("Error: Unable to load stats map!\n");
-			return XDP_ABORTED;
-		}
-		rec->rx_packets += 1;
-		rec->rx_bytes += ctx->data_end - ctx->data;
-
-		bpf_printk("%d from queue %d!\n", ret, rx_queue_idx);
+		ret = bpf_redirect_map(&xsks_map, rx_queue_idx, 0);
+		stats_add(ret, ctx->data_end - ctx->data);
         return ret;
 	}
 
 	bpf_printk("Skip: Not interested!\n");
+	stats_add(XDP_PASS, ctx->data_end - ctx->data);
 	return XDP_PASS;
 }
 
